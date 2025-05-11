c6afb6958704724302aabb6395d20d0a43cbe916
gekky27 <gekmasratihap@gmail.com>
2025-05-12 01:12:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// script.js
+
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector("nav ul").classList.toggle("show");
+});
+
+
+// Fungsi untuk tombol Donate
+document.getElementById("donateBtn").addEventListener("click", function () {
+    window.location.href = "donate.html"; // Ganti ke URL donasi yang sebenarnya jika ada
+  });
+  
+  // Fungsi untuk semua tombol Learn More
+  document.querySelectorAll(".card button").forEach(function (button) {
+  button.addEventListener("click", function () {
+    const targetPage = button.getAttribute("data-target");
+    if (targetPage) {
+      window.location.href = targetPage;
+    }
+  });
+});
+
+  
+// JavaScript for play button functionality
+document.querySelectorAll(".play-button").forEach(function (button) {
+  button.addEventListener("click", function () {
+    window.open("https://drive.google.com/file/d/1PqSdzpmdSryG73dbKaDWzz4Ms_I-ompP/preview", "_blank");
+  });
+});
+
+
+document.querySelectorAll(".join-btn").forEach(function (button) {
+  button.addEventListener("click", function () {
+      window.location.href = "https://www.example.com"; // Ganti ke halaman yang diinginkan
+  });
+});
